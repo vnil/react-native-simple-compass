1dfa34791dc53eb098500e41f1685d04813e2609
Kim A. Martinsen <ka.martinsen@gmail.com>
2018-01-19 15:03:59 +0100

    Remove requesting location when library opens

--- ios/RNSimpleCompass.m
+++ ios/RNSimpleCompass.m
@@ -15,10 +15,6 @@
         if ([CLLocationManager headingAvailable]) {
             self.locationManager = [[CLLocationManager alloc] init];
             self.locationManager.delegate = self;
-            if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
-                NSLog(@"Requesting permission");
-                [self.locationManager requestWhenInUseAuthorization];
-            }
         }
         else {
             NSLog(@"Heading not available");
